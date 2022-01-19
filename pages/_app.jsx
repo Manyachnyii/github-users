fc6584355c6cf9f9356ef5e9cda04a8fa2bb48ca
Manyachnyii <manyachnyii@gmail.com>
2022-01-19 11:34:58 +0300

    Add pageProps in component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App({ Component }) {
-  return (
-    <>
-      <Component />
-    </>
-  );
+export default function App({ Component, ...pageProps }) {
+  return <Component {...pageProps} />;
 }
