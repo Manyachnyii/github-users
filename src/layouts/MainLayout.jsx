64a1f84812720ed38214bc6d7b17384dec24d0d7
Manyachnyii <manyachnyii@gmail.com>
2022-01-19 08:57:43 +0300

    Add particles on background

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,74 @@
 import { Container } from "reactstrap";
+import Particles from "react-tsparticles";
 
 import Header from "../Header";
 
 import bg from "../assets/images/coming-soon.jpg";
 
 export default function MainLayout({ children }) {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <>
       <div className="wrapper">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 60,
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 4,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+
         <Container>
           <div className="grid-container">
             <header>
@@ -20,7 +81,7 @@ export default function MainLayout({ children }) {
 
       <style jsx>{`
         .wrapper {
-          background: center/cover no-repeat ${`url("${bg.src}")`};
+          background: ${`center/cover no-repeat url("${bg.src}")`};
         }
 
         .grid-container {
