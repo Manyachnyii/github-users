74122a14cc90e419bd0e9fa65d4b013be321b189
Manyachnyii <manyachnyii@gmail.com>
2022-01-19 11:50:10 +0300

    Add list of contacts

--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -1,9 +1,30 @@
 import MainLayout from "../src/layouts/MainLayout";
+import ContactList from "../src/ContactList";
 
-export default function Contacts() {
+export default function Contacts(props) {
+  const { users } = props.pageProps;
+
   return (
     <MainLayout>
       <h1>Страница со списком контактов</h1>
+      <ContactList users={users} />
     </MainLayout>
   );
 }
+
+export const getStaticProps = async (ctx) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  if (!users) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
---
+++ src/ContactList.jsx
@@ -0,0 +1,13 @@
+export default function ContactList({ users }) {
+  return (
+    <>
+      {users && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
